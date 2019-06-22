9e030a3f69724e3ced931f774a53cc4461df55b6
Aleksey Sazonov <salkvus@gmail.com>
2019-06-23 02:54:23 +0300

    Lesson 02 (v2)

--- lesson02/equation2.rb
+++ lesson02/equation2.rb
@@ -1,23 +1,24 @@
 print"Введите коэффициент a (при x2):"
-a = gets.chomp.to_f
+a = gets.to_f
 print"Введите коэффициент b (при x):"
-b = gets.chomp.to_f
+b = gets.to_f
 print"Введите коэффициент c (свободный член):"
-c = gets.chomp.to_f
+c = gets.to_f
 
-d = b**2 - 4*a*c
+d = b**2 - 4 * a  c
 if a == 0 && b == 0 && c != 0 || d < 0
-	abort "D = #{d} Корней нет"
+  abort "D = #{d} Корней нет"
 elsif a == 0 && b == 0 && c == 0
-	puts "Любое значение является решением этого уравнения (бесконечное множество решений)"
-	exit 0
+  puts "Любое значение является решением этого уравнения (бесконечное множество решений)"
+  exit 0
 end
 
 if d == 0
-	x1 = x2 = -b / (2.0 * a)
-	puts "D = #{d} #{x1}"
+  x1 = x2 = -b / (2.0 * a)
+  puts "D = #{d} #{x1}"
 else
-	x1 = (-b + Math.sqrt(d)) / (2.0 * a)
-	x2 = (-b + Math.sqrt(d)) / (2.0 * a)
-	puts "D = #{d} #{x1} #{x2}"
+  d_sqrt = Math.sqrt(d)
+  x1 = (-b - d_sqrt / (2.0 * a)
+  x2 = (-b + d_sqrt / (2.0 * a)
+  puts "D = #{d} #{x1} #{x2}"
 end
