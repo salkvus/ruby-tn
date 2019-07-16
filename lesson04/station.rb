a58026966fd0efead6cd0ecb1f327a2e3e6a1ded
Aleksey Sazonov <salkvus@gmail.com>
2019-07-16 21:11:09 +0300

    Lesson04 (v1)

---
+++ lesson04/station.rb
@@ -0,0 +1,126 @@
+class Station
+  attr_reader :name, :trains
+
+  def initialize(name)
+    @name = name
+    @trains = {}
+  end
+
+  def arrive_train(train)
+    type = train.type
+    trains_list = trains[type] ||= []
+    trains_list << train unless trains_list.include?(train)
+    train.station = self
+  end
+  
+  def departure_train(train)
+    trains[train.type].delete(train)
+    train.station = self
+  end
+
+  def show_trains
+    trains.each do |type, trains_list| 
+      trains_list.each { |train| train.show }
+    end
+  end
+
+  def show_trains_by_type(type)
+    trains[type].each { |train| train.show }
+  end
+
+end
+
+class Route
+  attr_reader :stations
+
+  def initialize(start_station, end_station)
+    @stations = []
+    @stations << start_station << end_station
+  end
+
+  def start_station
+    stations[0] if stations.length > 0
+  end
+
+  def end_station
+    stations[-1] if stations.length > 0
+  end
+
+  def add(station)
+    @stations = @stations[0..-2] << station << @stations[-1]
+  end
+
+  def delete(station)
+    stations.delete(station) if stations.length > 2 && station != start_station && station != end_station
+  end
+
+  def show
+    stations.each { |station| puts station.name }
+  end
+end
+
+class Train
+  TYPES = %i[cargo passenger]
+  attr_reader :number, :type, :length, :route, :speed, :TYPES
+  attr_accessor :station
+  
+  def initialize(number, type, train_length = 0)
+    raise TrainTypeException.new(type, TYPES) unless TYPES.include?(type)
+
+    @number = number
+    @type = type
+    @length = train_length
+    @speed = 0
+    @station = nil
+  end
+
+  def show
+    puts "Train num. #{self.number}, type: #{self.type}, length: #{self.length}"
+  end
+
+  def hook_carriage
+    @length += 1 if speed.zero?
+  end
+
+  def unhook_carriage
+    @length -= 1 if speed.zero? && length.positive?
+  end
+
+  def accelerate
+    @speed += 20 if speed < 80
+  end
+
+  def stop
+    @speed = 0
+  end
+
+  def route=(route)
+    @route = route
+    self.station = route.start_station
+  end
+
+  def go_back
+    previous_station
+    station.arrive_train(self)
+  end
+
+  def go_forward
+    next_station
+    station.arrive_train(self)
+  end
+
+  def previous_station
+    self.station = route.stations[route.stations.index(station) - 1] unless station == route.start_station
+  end
+
+  def next_station
+    self.station = route.stations[route.stations.index(station) + 1] unless station == route.end_station
+  end
+end
+
+class TrainTypeException < StandardError
+  def initialize(type, error_types)
+    puts "Wrong train type '#{type}'"
+    puts "Valid types #{error_types}"
+  end
+end
---
+++ lesson04/test.rb
@@ -0,0 +1,55 @@
+require_relative 'station'
+
+st1 = Station.new('Moscow')
+st2 = Station.new('Saint-Petersburg')
+st3 = Station.new('Pskov')
+st4 = Station.new('Kiev')
+
+rt1 = Route.new(st1, st4)
+puts "start_station: #{rt1.start_station.name}"
+puts "end_station: #{rt1.end_station.name}"
+puts "\"Adding #{st2.name} #{st3.name}\""
+rt1.add(st2)
+rt1.add(st3)
+rt1.show()
+puts "\"Deleting #{st3.name}\""
+rt1.delete(st3)
+rt1.show()
+
+tr1 = Train.new("11", :passenger, 10)
+puts "Assigning route"
+tr1.route = rt1
+puts "Train station: #{tr1.station.name}"
+puts "Hooking 2 carriages"
+tr1.hook_carriage()
+tr1.hook_carriage()
+puts "Train length: #{tr1.length}"
+puts "Unhooking 1 carriage"
+tr1.unhook_carriage()
+puts "Train length: #{tr1.length}"
+puts "Train speed: #{tr1.speed}"
+puts "Accelerating train ..."
+tr1.accelerate()
+puts "Train speed: #{tr1.speed}"
+tr1.accelerate()
+puts "Train speed: #{tr1.speed}"
+puts "Go forward ..."
+tr1.go_forward()
+puts "Train station: #{tr1.station.name}"
+puts "Go forward ..."
+tr1.go_forward()
+puts "Train station: #{tr1.station.name}"
+puts "Go back ..."
+tr1.go_back()
+
+tr2 = Train.new("22", :cargo, 20)
+rt2 = Route.new(st1, st3)
+rt2.add(st2)
+st2.arrive_train(tr2)
+
+puts "Train #{tr1.number} station: #{ tr1.station.name}"
+puts "Train #{tr2.number} station: #{ tr2.station.name}"
+puts "Show all trains:"
+st2.show_trains
+puts "Show trains by type:"
+st2.show_trains_by_type(:cargo)
